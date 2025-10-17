d2a6401ab3b7eb56c1d1806fec5b0bc5490ce1da
twindoodles <47507866+Twindoodles@users.noreply.github.com>
2025-10-18 01:10:15 +0800

    added playRound()

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function getComputerChoice(){
 }
 
 function getHumanChoice(){
-    let choice = prompt("Rock, paper, scissors! (I choose...)");
+    let choice = prompt("Let's play rock, paper, scissors!");
     choice = choice.toLowerCase();
     if (choice == "rock") {
         return choice
@@ -24,8 +24,35 @@ function getHumanChoice(){
     }
 }
 
-console.log(getHumanChoice());
-
 function playRound(humanChoice, computerChoice){
-    //returns choice based on input
+    if (humanChoice == computerChoice){
+        return "Tie!"
+    } else if ((humanChoice == "rock" || computerChoice == "rock") && (humanChoice == "scissors" || computerChoice == "scissors")) {
+        if (humanChoice == "rock") {
+            humanScore += 1;
+            return "Rock beats scissors. You won!"
+        } else {
+            computerScore += 1;
+            return "Rock beats scissors. You lost :)"
+        }
+    } else if (humanChoice.length > computerChoice.length) {
+        humanScore += 1;
+        return `${capitalize(humanChoice)} beats ${computerChoice}. You won!`
+    } else {
+        computerScore += 1;
+        return `${capitalize(computerChoice)} beats ${humanChoice}. You lost :)`
+    }
+    }
+    
+function capitalize(string){
+    let allLower = string.toLowerCase();
+    let restLower = allLower.slice(1,string.length);
+    let firstLetter = string.charAt(0);
+    let capitalLetter = firstLetter.toUpperCase();
+    return capitalLetter + restLower;
 }
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection));
